c5ae82637e54da1fce372a6a1193108956c8db70
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-02-16 02:51:01 -0600

    add sample content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @content_first = "This is some sample text from index post controller"
+    @content_second = "This is a second content_second"
+  end
 
+  def new
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
   end
 
 end
