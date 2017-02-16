53d77d8861b330d1cc0dbc2905106cf5302bd663
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-02-16 14:19:50 -0600

    Add new post and edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,19 +8,49 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new()
+    @category = Category.all
   end
 
   def create
+    #Initialize  model with attributes
+    @post = Post.new(post_params)
+
+    # Save in database
+    if @post.save
+      #Create flash message
+      flash[:notice] = "Post successfully created"
+
+      #Redirect to main resource pages
+      redirect_to posts_path
+    else
+      #Show form again
+      render 'new'
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path, :notice => "Your post has been updated"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "Your post has been deleted"
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :category_id, :body)
+    end
 
 end
